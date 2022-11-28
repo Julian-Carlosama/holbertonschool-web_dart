4b4f8d0f2884fe1d23524fdc8a0a95154b50913e
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-11-28 17:26:21 -0500

    Add task 5

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  /*
+  Write Your code below 
+   */
+  var nb = args.length;
+
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
+}
