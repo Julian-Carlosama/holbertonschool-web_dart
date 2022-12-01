24f96a17c8c2c3fe91987c5c23dbde79c38e7ec1
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-11-30 19:34:35 -0500

    Add files done

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a + b;
+}
+
+String showFunc(int a, int b) {
+  String addStr = 'Add $a + $b = ${add(a, b)}';
+  String subStr = 'Sub $a - $b = ${sub(a, b)}';
+  return addStr + '\n' + subStr;
+}
