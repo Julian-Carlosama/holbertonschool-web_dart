bf249a2af6bec615e613c69a985e51ddc65f7aed
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-12-02 18:34:22 -0500

    Task 4 done

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,27 @@
+import '2-password.dart';
+
+class Password {
+
+  // Private property
+  String _password = '';
+
+  // Constructor that define the private properties
+  Password({required String password}) : _password = password;
+
+  // Get Method that allow access to private properties 
+  String get passw => _password;
+
+  // Set Method that allow set private properties
+  set password(String passw) => _password = passw;
+
+  bool isValid() {
+    return ((this.passw.length >= 8 && this.passw.length <= 16) &&
+            this.passw.contains(RegExp(r'[A-Z]')) &&
+            this.passw.contains(RegExp(r'[a-z]')) &&
+            this.passw.contains(RegExp(r'[0-9]'))
+        ? true
+        : false);
+  }
+
+  String toString() => 'Your Password is: ${this.passw}';
+}
