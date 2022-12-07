cff1a2a00d4cd0f46f18da553dfbcd92105926c5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-12-06 23:37:51 -0500

    Updated task

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,15 +3,18 @@ import '4-main.dart';
 
 Future<double> calculateTotal() async {
   try {
-    final id = jsonDecode(await fetchUserid());
-    final prtds = jsonDecode(await fetchUserOrders(id['id']));
-
-    double finalPrice = 0;
-
-    for (String product in prtds) {
-      finalPrice += jsonDecode(await fetchProductPrice(product));
-    }
-    return finalPrice;
+    return fetchUserData().then((user) {
+      String id = json.decode(user)["id"];
+      return fetchUserOrders(id).then((products) async {
+        double finalPrice = 0.0;
+        for (String product in json.decode(products)) {
+          await fetchProductPrice(product).then((price) {
+            finalPrice += json.decode(price);
+          });
+        }
+        return finalPrice;
+      });
+    });
   } catch (err) {
     return -1;
   }
